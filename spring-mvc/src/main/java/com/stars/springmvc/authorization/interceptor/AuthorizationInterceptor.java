beb977d129a2656492190d70b87cdd71ea43be6c
Guilin <guilinw98@gmail.com>
2023-01-10 11:33:46 +0800

    mvc interceptor

--- spring-mvc/src/main/java/com/stars/springmvc/authorization/interceptor/AuthorizationInterceptor.java
+++ spring-mvc/src/main/java/com/stars/springmvc/authorization/interceptor/AuthorizationInterceptor.java
@@ -2,6 +2,9 @@ package com.stars.springmvc.authorization.interceptor;
 
 import com.stars.springmvc.authorization.annotation.Authorization;
 import com.stars.springmvc.authorization.config.Constants;
+import com.stars.springmvc.authorization.manager.TokenManager;
+import com.stars.springmvc.authorization.model.TokenModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -19,6 +22,8 @@ import java.lang.reflect.Method;
 @Component
 public class AuthorizationInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    private TokenManager manager;
 
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //如果不是映射到方法直接通过
@@ -29,6 +34,13 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
         Method method = handlerMethod.getMethod();
         //从header中得到token
         String authorization = request.getHeader(Constants.AUTHORIZATION);
+        //验证token
+        TokenModel model = manager.getToken(authorization);
+        if (manager.checkToken(model)) {
+            //如果token验证成功，将token对应的用户id存在request中，便于之后注入
+            request.setAttribute(Constants.CURRENT_USER_ID, model.getUserId());
+            return true;
+        }
 
         if (method.getAnnotation(Authorization.class) != null) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
--- spring-mvc/src/main/java/com/stars/springmvc/controller/AuthorizationController.java
+++ spring-mvc/src/main/java/com/stars/springmvc/controller/AuthorizationController.java
@@ -1,12 +1,18 @@
 package com.stars.springmvc.controller;
 
 import com.stars.springmvc.authorization.annotation.Authorization;
+import com.stars.springmvc.authorization.annotation.CurrentUser;
+import com.stars.springmvc.authorization.manager.TokenManager;
+import com.stars.springmvc.authorization.model.TokenModel;
+import com.stars.springmvc.constant.ResultStatus;
+import com.stars.springmvc.domain.User;
 import com.stars.springmvc.model.ResultModel;
+import com.stars.springmvc.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Description:
@@ -18,10 +24,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/author")
 public class AuthorizationController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TokenManager tokenManager;
+
+
     @GetMapping
     @Authorization
     public ResponseEntity<ResultModel> hello(String name){
         String result = "hello world,"+name;
         return new ResponseEntity<>(ResultModel.ok(result), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<ResultModel> login(@RequestParam String username, @RequestParam String password) {
+        Assert.notNull(username, "username can not be empty");
+        Assert.notNull(password, "password can not be empty");
+
+        User user = userRepository.findByUsername(username);
+        if (user == null ||  //未注册
+                !user.getPassword().equals(password)) {  //密码错误
+            //提示用户名或密码错误
+            return new ResponseEntity<>(ResultModel.error(ResultStatus.USERNAME_OR_PASSWORD_ERROR), HttpStatus.NOT_FOUND);
+        }
+        //生成一个token，保存用户登录状态
+        TokenModel model = tokenManager.createToken(user.getId());
+        return new ResponseEntity<>(ResultModel.ok(model), HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    @Authorization
+    public ResponseEntity<ResultModel> logout(@CurrentUser User user) {
+        tokenManager.deleteToken(user.getId());
+        return new ResponseEntity<>(ResultModel.ok(), HttpStatus.OK);
     }
 }
