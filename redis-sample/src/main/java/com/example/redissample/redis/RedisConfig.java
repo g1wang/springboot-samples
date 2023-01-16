89b82110c10057cf13915fd9ac8c6cdda0942e04
Guilin <guilinw98@gmail.com>
2023-01-16 17:27:51 +0800

    mvc interceptor

---
+++ redis-sample/src/main/java/com/example/redissample/redis/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.example.redissample.redis;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
+
+/**
+ * @Description:
+ * @Author laboratory
+ * @Date 2023/1/16
+ */
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+}
