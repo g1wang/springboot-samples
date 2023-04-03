8dd9161a8553ea59e1e6935a95a97886b45b9777
Guilin <guilinw98@gmail.com>
2023-04-03 16:41:38 +0800

    swagger

---
+++ swagger-samples/src/main/java/com/stars/swaggersamples/SwaggerSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.stars.swaggersamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerSamplesApplication.class, args);
+    }
+
+}
---
+++ swagger-samples/src/test/java/com/stars/swaggersamples/SwaggerSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stars.swaggersamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
